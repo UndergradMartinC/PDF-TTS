3c95d005a99d4d09d73218de05b4825fd902238b
Mcook-UL <c00102798@louisiana.edu>
2025-05-12 11:25:03 -0500

    added file selection button functionality, added volume and speed sliders and respective functions

--- main.py
+++ main.py
@@ -10,6 +10,12 @@ def getFile():
 
     playFile(file)
 
+def setVolume(percentage):
+    player.setProperty("volume", int(percentage)/100)
+
+def setSpeed(wordsPerMinute):
+    player.setProperty("rate", int(wordsPerMinute))
+
 def playFile(pdfFile):
 
     pdfreader = PyPDF2.PdfReader(pdfFile)
@@ -19,10 +25,10 @@ def playFile(pdfFile):
         page = pdfreader.pages[num]
         text = page.extract_text()
 
-        player = pyttsx3.init()
         player.say(text)
         player.runAndWait()
 
+player = pyttsx3.init()
 
 window = Tk()
 window.geometry("800x600")
@@ -30,11 +36,25 @@ window.title("PDF TTS")
 window.config(background="black")
 
 main_font = tkFont.Font(family="Arial", size=25)
+secondary_font = tkFont.Font(family="Arial", size=15)
 
-testLabel = Label(window, text="Click the button to get started",background="black", fg="green", font=main_font)
-testLabel.pack()
+buttonLabel = Label(window, text="Click the button to get started",background="black", fg="green", font=main_font)
+buttonLabel.pack()
 
-fileButton = Button(name="file", text="Select PDF", font=main_font)
+fileButton = Button(window, name="file", text="Select PDF", font=main_font, command=getFile)
 fileButton.pack()
+
+volumeLabel = Label(window, text="Volume", font=secondary_font, background="black", fg="green")
+volumeLabel.pack()
+
+volumeSlider = Scale(window, from_=0, to=100, orient="horizontal", command=setVolume)
+volumeSlider.pack()
+
+speedLabel = Label(window, text="Speed(Words per minute)", font=secondary_font, background="black", fg="green")
+speedLabel.pack()
+
+speedSlider = volumeSlider = Scale(window, from_=150, to=300, orient="horizontal", command=setSpeed)
+speedSlider.pack()
+
 
 window.mainloop()
