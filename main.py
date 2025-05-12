1cdc3e9f6c1b3afd6c96f70601aa945f24b1b4a6
Mcook-UL <c00102798@louisiana.edu>
2025-05-12 07:42:35 -0500

    added label, button and main font. wrapped functional code in functions for button

--- main.py
+++ main.py
@@ -2,18 +2,39 @@ import pyttsx3
 import PyPDF2
 from tkinter.filedialog import *
 from tkinter import *
+import tkinter.font as tkFont
 
 
+def getFile():
+    file = askopenfilename(filetypes=[("PDF file", "*.pdf")])
 
-file = askopenfilename()
+    playFile(file)
 
-pdfreader = PyPDF2.PdfReader(file)
-numPages = len(pdfreader.pages)
+def playFile(pdfFile):
 
-for num in range(0,numPages):
-    page = pdfreader.pages[num]
-    text = page.extract_text()
+    pdfreader = PyPDF2.PdfReader(pdfFile)
+    numPages = len(pdfreader.pages)
 
-    player = pyttsx3.init()
-    player.say(text)
-    player.runAndWait()
+    for num in range(0,numPages):
+        page = pdfreader.pages[num]
+        text = page.extract_text()
+
+        player = pyttsx3.init()
+        player.say(text)
+        player.runAndWait()
+
+
+window = Tk()
+window.geometry("800x600")
+window.title("PDF TTS")
+window.config(background="black")
+
+main_font = tkFont.Font(family="Arial", size=25)
+
+testLabel = Label(window, text="Click the button to get started",background="black", fg="green", font=main_font)
+testLabel.pack()
+
+fileButton = Button(name="file", text="Select PDF", font=main_font)
+fileButton.pack()
+
+window.mainloop()
